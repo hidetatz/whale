3075bdb35b5a616f2112c87a7c10e8efe55e678f
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-11-26 20:31:21 +0900

    wip implement tensor broadcasting

--- tensor.go
+++ tensor.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Tensor struct {
-	val   []float64
+	data  []float64
 	shape []int
 	dim   int
 }
 
 func TensorByScalar(data float64) *Tensor {
-	return &Tensor{val: []float64{data}}
+	return &Tensor{data: []float64{data}}
 }
 
 func TensorNd(data []float64, shape ...int) *Tensor {
-	return &Tensor{val: data, shape: shape, dim: len(shape)}
+	return &Tensor{data: data, shape: shape, dim: len(shape)}
 }
 
 func Zeros(shape ...int) *Tensor {
@@ -40,6 +40,129 @@ func Ones(shape ...int) *Tensor {
 	return TensorNd(data, shape...)
 }
 
+func (t *Tensor) Equals(t2 *Tensor) bool {
+	if !t.SameShape(t2) {
+		return false
+	}
+
+	if t.dim != t2.dim {
+		return false
+	}
+
+	if len(t.data) != len(t2.data) {
+		return false
+	}
+
+	for i := range t.data {
+		if t.data[i] != t2.data[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (t *Tensor) SameShape(t2 *Tensor) bool {
+	if len(t.shape) != len(t2.shape) {
+		return false
+	}
+
+	for i := range t.shape {
+		if t.shape[i] != t2.shape[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (t *Tensor) Reshape(shape ...int) (*Tensor, error) {
+	if total(t.shape) != total(shape) {
+		return nil, fmt.Errorf("cannot reshape: the data size mismatch")
+	}
+
+	nd := make([]float64, len(t.data))
+	copy(nd, t.data)
+	nt := &Tensor{
+		data:  nd,
+		shape: shape,
+		dim:   len(shape),
+	}
+	return nt, nil
+}
+
+func total(shape []int) int {
+	total := 1
+	for _, dim := range shape {
+		total *= dim
+	}
+	return total
+}
+
+func (t *Tensor) Tile(newShape ...int) (*Tensor, error) {
+	newSize := total(newShape)
+	newData := make([]float64, newSize)
+
+	broadcastIndex := func(idx int, shape []int) int {
+		var result int
+		for i, s := range shape {
+			result += (idx / total(shape[i+1:])) % s * total(newShape[i+1:])
+		}
+		return result
+	}
+
+	for i := 0; i < newSize; i++ {
+		sourceIndex := broadcastIndex(i, newShape)
+		newData[i] = t.data[sourceIndex]
+	}
+
+	// for i := 0; i < len(newShape); i++ {
+	// 	if newShape[i] == t.shape[i] {
+	// 		continue
+	// 	}
+
+	// 	if t.shape[i] == 1 {
+	// 		for j := 0; j < t.shape[i]; j++ {
+	// 			copy(newData[j*newSize:(j+1)*newSize], t.data[j*total(t.shape[i+1:]):(j+1)*total(t.shape[i+1:])])
+	// 		}
+
+	// 		continue
+	// 	}
+
+	// 	return nil, fmt.Errorf("cannot tile: the length of %dd is %d, but 1 is expected.", i, newShape[i])
+	// }
+
+	return &Tensor{data: newData, shape: newShape, dim: len(newShape)}, nil
+}
+
+func (t *Tensor) BroadcastTo(shape ...int) (*Tensor, error) {
+	if len(t.shape) > len(shape) {
+		return nil, fmt.Errorf("cannot broadcast: ndim must be smaller then the given one")
+	}
+
+	newshape := make([]int, len(t.shape))
+	copy(newshape, t.shape)
+	if len(t.shape) != len(shape) {
+		delta := len(shape) - len(newshape)
+		for i := 0; i < delta; i++ {
+			// push 1 to the head until the dim gets the same
+			newshape = append([]int{1}, newshape...)
+		}
+	}
+
+	nt, err := t.Reshape(newshape...)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(nt, nt.data, nt.shape, nt.dim, shape)
+	nt, err = nt.Tile(shape...)
+	if err != nil {
+		return nil, err
+	}
+
+	return nt, nil
+}
+
 func (t *Tensor) String() string {
 	s := ""
 
@@ -61,12 +184,12 @@ func (t *Tensor) String() string {
 		for i := 0; i < size; i++ {
 			newIndent := strings.Repeat("  ", depth)
 			s += fmt.Sprintf("%s[\n", indent)
-			printTensorRec(data[i*size:(i+1)*size], shape[1:], depth+1, newIndent)
+			printTensorRec(data[i*total(shape[1:]):(i+1)*total(shape[1:])], shape[1:], depth+1, newIndent)
 			s += fmt.Sprintf("%s]\n", indent)
 		}
 	}
 
-	printTensorRec(t.val, t.shape, 1, "")
+	printTensorRec(t.data, t.shape, 1, "")
 	return s
 }
 
--- tensor_test.go
+++ tensor_test.go
@@ -1,11 +1,23 @@
 package whale
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestTensorString(t *testing.T) {
+	ts := TensorNd([]float64{1, 2}, 2)
+	got, err := ts.BroadcastTo(2, 2, 2)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	expected := TensorNd([]float64{1, 2, 1, 2, 1, 2, 1, 2}, 2, 2, 2)
+	if !ts.Equals(expected) {
+		t.Errorf("expected: '%s', got: '%s'", expected, got)
+	}
+}
+
+func TestTensor_String(t *testing.T) {
 	ts := Ones(2, 3, 3)
 
 	expected := `[
@@ -28,6 +40,19 @@ func TestTensorString(t *testing.T) {
   ]
   [
     [1 1 1]
+  ]
+]
+`
+
+	if ts.String() != expected {
+		t.Errorf("expected: '%s', got: '%s'", expected, ts.String())
+	}
+
+	ts = TensorNd([]float64{1, 2}, 1, 1, 2)
+
+	expected = `[
+  [
+    [1 2]
   ]
 ]
 `
