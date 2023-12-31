5d4f33260d06d0d4af56c076fc281d001eb6699b
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-12-31 22:22:40 +0900

    simplify test using reflect

--- operation_test.go
+++ operation_test.go
@@ -1,6 +1,7 @@
 package whale
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/hidetatz/whale/tensor"
@@ -59,13 +60,17 @@ func vs(vs ...*Variable) []*Variable {
 	return vs
 }
 
-func TestReshape(t *testing.T) {
+func TestOperations(t *testing.T) {
 	tests := []struct {
-		name  string
-		in    *tensor.Tensor
-		shape []int
+		name string
 
-		// verification
+		fn any
+		in *tensor.Tensor
+		// extra arguments to the fn
+		extra []any
+		// flag if the last param of arguments is variable length
+		variable bool
+
 		expected *tensor.Tensor
 		grad     *tensor.Tensor
 
@@ -74,42 +79,16 @@ func TestReshape(t *testing.T) {
 	}{
 		{
 			name:     "reshape",
+			fn:       Reshape,
 			in:       arng(t, 1, 13, 2, 2, 3),
-			shape:    []int{6, 2},
+			extra:    []any{[]int{6, 2}},
+			variable: true,
 			expected: arng(t, 1, 13, 6, 2),
 			grad:     ones(t, 2, 2, 3),
 		},
-	}
-
-	for _, tc := range tests {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			x := NewVar(tc.in)
-			y, err := Reshape(x, tc.shape...)
-			if err != nil {
-				t.Errorf("unexpected err on forward: %v", err)
-			}
-
-			verify(t, vs(x), vs(y), ts(tc.expected), ts(tc.grad))
-		})
-	}
-}
-
-func TestTranspose(t *testing.T) {
-	tests := []struct {
-		name string
-		in   *tensor.Tensor
-
-		// verification
-		expected *tensor.Tensor
-		grad     *tensor.Tensor
-
-		// dezero verification
-		dezero string
-	}{
 		{
-			name:     "reshape",
+			name:     "transpose",
+			fn:       Transpose,
 			in:       arng(t, 1, 13, 2, 2, 3),
 			expected: arng(t, 1, 13, 2, 2, 3).Transpose(),
 			grad:     ones(t, 2, 2, 3),
@@ -121,14 +100,40 @@ func TestTranspose(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			t.Log(tc.in.RawString())
-			t.Log(tc.expected.RawString())
-			t.Log(tc.grad.RawString())
-
 			x := NewVar(tc.in)
-			y, err := Transpose(x)
-			if err != nil {
-				t.Errorf("unexpected err on forward: %v", err)
+
+			f := reflect.ValueOf(tc.fn)
+
+			args := []reflect.Value{reflect.ValueOf(x)}
+			for _, e := range tc.extra {
+				args = append(args, reflect.ValueOf(e))
+			}
+
+			var results []reflect.Value
+			if tc.variable {
+				results = f.CallSlice(args)
+			} else {
+				results = f.Call(args)
+			}
+
+			if len(results) != 2 {
+				t.Errorf("unexpected return length: %v", len(results))
+			}
+
+			y, ok := results[0].Interface().(*Variable)
+			if !ok {
+				t.Errorf("unexpected first-returned value: %v", results[0])
+			}
+
+			if !results[1].IsNil() {
+				err, ok := results[1].Interface().(error)
+				if !ok {
+					t.Errorf("unexpected second-returned value: %v", results[1])
+				}
+
+				if err != nil {
+					t.Errorf("unexpected err on forward: %v", err)
+				}
 			}
 
 			verify(t, vs(x), vs(y), ts(tc.expected), ts(tc.grad))
