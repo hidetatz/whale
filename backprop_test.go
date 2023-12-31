b87f13a944adb06ae7d7bf2e18ff8db7bf899669
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-12-31 15:06:53 +0900

    write some tests

--- backprop_test.go
+++ backprop_test.go
@@ -1,173 +1,137 @@
 package whale
 
 import (
-	// "fmt"
-	"math"
 	"testing"
 
 	"github.com/hidetatz/whale/tensor"
 )
 
-func TestOps(t *testing.T) {
+func arng(t *testing.T, from, to int, shape ...int) *tensor.Tensor {
+	t.Helper()
+	tsr, _ := tensor.ArangeFrom(from, to).Reshape(shape...)
+	return tsr
+}
+
+func ones(t *testing.T, shape ...int) *tensor.Tensor {
+	t.Helper()
+	return tensor.Ones(shape...)
+}
+
+func verify(t *testing.T, in, out []*Variable, expected, expectedGrad []*tensor.Tensor) {
+	t.Helper()
+
+	// check calc output
+	if len(expected) != len(out) {
+		t.Errorf("result length mismatch expected %v but got %v", len(expected), len(out))
+	}
+
+	for i := range expected {
+		if !expected[i].Equals(out[i].data) {
+			t.Errorf("expected %v but got %v", expected[i], out[i].data)
+		}
+	}
+
+	// check gradient
+	for _, o := range out {
+		if err := o.Backward(); err != nil {
+			t.Errorf("unexpected err on backward: %v", err)
+		}
+	}
+
+	if len(expectedGrad) != len(in) {
+		t.Errorf("grad length mismatch expected %v but got %v", len(expectedGrad), len(in))
+	}
+
+	for i := range expectedGrad {
+		if !expectedGrad[i].Equals(in[i].grad.data) {
+			t.Errorf("grad: expected %v but got %v", expectedGrad[i].RawString(), in[i].grad.data.RawString())
+		}
+	}
+}
+
+// makes slice of tensors
+func ts(tensors ...*tensor.Tensor) []*tensor.Tensor {
+	return tensors
+}
+
+// makes slice of variable
+func vs(vs ...*Variable) []*Variable {
+	return vs
+}
+
+func TestReshape(t *testing.T) {
 	tests := []struct {
-		name string
-		f    func([]*Variable) []*Variable
-		x    []*Variable
+		name     string
+		in       *tensor.Tensor
+		shape    []int
+
+		// verification
+		expected *tensor.Tensor
+		grad *tensor.Tensor
+
+		// dezero verification
+		dezero string
 	}{
 		{
-			name: "square",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Square_(x[0])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2))},
+			name:     "reshape",
+			in:       arng(t, 1, 13, 2, 2, 3),
+			shape:    []int{6, 2},
+			expected: arng(t, 1, 13, 6, 2),
+			grad:     ones(t, 2, 2, 3),
 		},
-		{
-			name: "exp",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Exp_(x[0])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2))},
-		},
-		{
-			name: "add",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Add_(x[0], x[1])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2)), NewVar(tensor.FromScalar(3))},
-		},
-		{
-			name: "sub",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Sub_(x[0], x[1])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2)), NewVar(tensor.FromScalar(3))},
-		},
-		{
-			name: "mul",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Mul_(x[0], x[1])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2)), NewVar(tensor.FromScalar(3))},
-		},
-		{
-			name: "div",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Div_(x[0], x[1])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2)), NewVar(tensor.FromScalar(3))},
-		},
-		{
-			name: "neg",
-			f:    func(x []*Variable) []*Variable { return []*Variable{Neg_(x[0])} },
-			x:    []*Variable{NewVar(tensor.FromScalar(2))},
-		},
-		{
-			name: "pow",
-			// pow (a^b) requires a and b but grad is calculated only for a.
-			// If b is included in x below, then this test code fails when calculating diff on b,
-			// so b is directly passed to pow(), instead of being included in x.
-			f: func(x []*Variable) []*Variable { return []*Variable{Pow_(x[0], NewVar(tensor.FromScalar(3)))} },
-			x: []*Variable{NewVar(tensor.FromScalar(2))},
-		},
-		{
-			name: "combined 1",
-			f: func(x []*Variable) []*Variable {
-				a := Add_(x[0], x[1])
-				b := Mul_(a, x[2])
-				y := Div_(b, x[3])
-				return []*Variable{y}
-			},
-			x: []*Variable{NewVar(tensor.FromScalar(2)), NewVar(tensor.FromScalar(3)), NewVar(tensor.FromScalar(4)), NewVar(tensor.FromScalar(5))},
-		},
-		{
-			name: "sphere",
-			f: func(x []*Variable) []*Variable {
-				y := Add_(Square_(x[0]), Square_(x[1]))
-				return []*Variable{y}
-			},
-			x: []*Variable{NewVar(tensor.FromScalar(1)), NewVar(tensor.FromScalar(1))},
-		},
-		{
-			name: "matyas",
-			f: func(x []*Variable) []*Variable {
-				t1 := Pow_(x[0], NewVar(tensor.FromScalar(2)))
-				t2 := Pow_(x[1], NewVar(tensor.FromScalar(2)))
-				t3 := Mul_(NewVar(tensor.FromScalar(0.26)), Add_(t1, t2))
-				t4 := Mul_(NewVar(tensor.FromScalar(0.48)), x[0])
-				t5 := Mul_(t4, x[1])
-				y := Sub_(t3, t5)
-				return []*Variable{y}
-			},
-			x: []*Variable{NewVar(tensor.FromScalar(1)), NewVar(tensor.FromScalar(1))},
-		},
-		// {
-		// 	name: "matyas",
-		// 	f: "0.26 * (x[0] ** 2 + x[1] ++ 2) - 0.48 * x[0] * x[1]"
-		// 	x: []*Variable{NewVar(tensor.FromScalar(1)), NewVar(tensor.FromScalar(1))},
-		// },
 	}
 
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			y := tc.f(tc.x)[0]
-			y.Backward()
-
-			grad := numDiff(t, tc.f, tc.x)
-			for i := range grad {
-				delta := math.Abs(Sub_(tc.x[i].grad, NewVar(grad[i])).data.Data[0])
-				if delta > 1e-4 {
-					t.Errorf("backward: %v, numerical diff: %v", tc.x[i].grad.data.Data[0], grad[i].Data[0])
-				}
+			x := NewVar(tc.in)
+			y, err := Reshape(x, tc.shape...)
+			if err != nil {
+				t.Errorf("unexpected err on forward: %v", err)
 			}
+
+			verify(t, vs(x), vs(y), ts(tc.expected), ts(tc.grad))
 		})
 	}
 }
 
-// numDiff differentiates x with f by numerical propagation for testing purpose
-func numDiff(t *testing.T, f func([]*Variable) []*Variable, xs []*Variable) []*tensor.Tensor {
-	t.Helper()
-	h := 1e-4
-	grad := []*tensor.Tensor{}
+func TestTranspose(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       *tensor.Tensor
 
-	// Do partial differentiation by changing only one x at a time.
-	// We use this formula for each x: (f(x1 + h, x2, ...)  - f(x1 - h, x2, ...)) / 2h
-	for i := range xs {
-		// save current focusing x to restore later.
-		tmp := xs[i]
+		// verification
+		expected *tensor.Tensor
+		grad *tensor.Tensor
 
-		// First calculate f(x1 + h, x2, ...)
-		xs[i].data = device.Add(xs[i].data, tensor.FromScalar(h))
-		y1 := f(xs)
-
-		// Second, calculate f(x1 - h, x2, ...)
-		// h * 2 is required because xs[i].data is already added above
-		xs[i].data = device.Sub(xs[i].data, tensor.FromScalar(h*2))
-		y2 := f(xs)
-
-		// At last do (y1 - y2) / 2h. This is the gradient
-		grad = append(grad, device.Div(device.Sub(y1[0].data, y2[0].data), tensor.FromScalar(2*h)))
-
-		// restore the original x
-		xs[i] = tmp
+		// dezero verification
+		dezero string
+	}{
+		{
+			name:     "reshape",
+			in:       arng(t, 1, 13, 2, 2, 3),
+			expected: arng(t, 1, 13, 2, 2, 3).Transpose(),
+			grad:     ones(t, 2, 2, 3),
+		},
 	}
 
-	return grad
-}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-func TestHigherOrderBackprop(t *testing.T) {
-	f := func(x []*Variable) []*Variable {
-		t1 := Pow_(x[0], NewVar(tensor.FromScalar(4)))
-		t2 := Pow_(x[0], NewVar(tensor.FromScalar(2)))
-		t3 := Mul_(NewVar(tensor.FromScalar(2)), t2)
-		y := Sub_(t1, t3)
-		return []*Variable{y}
-	}
-	x := []*Variable{NewVar(tensor.FromScalar(2))}
+			t.Log(tc.in.RawString())
+			t.Log(tc.expected.RawString())
+			t.Log(tc.grad.RawString())
 
-	for i := 0; i < 10; i++ {
-		y := f(x)[0]
-		x[0].ClearGrad()
-		y.Backward()
+			x := NewVar(tc.in)
+			y, err := Transpose(x)
+			if err != nil {
+				t.Errorf("unexpected err on forward: %v", err)
+			}
 
-		gx := x[0].GetGrad()
-		x[0].ClearGrad()
-		gx.Backward()
-		gx2 := x[0].GetGrad()
-
-		x[0].SetData(device.Sub(x[0].GetData(), device.Div(gx.GetData(), gx2.GetData())))
-	}
-
-	if x[0].GetData().Data[0] != 1 {
-		t.Errorf("failed!")
+			verify(t, vs(x), vs(y), ts(tc.expected), ts(tc.grad))
+		})
 	}
 }
