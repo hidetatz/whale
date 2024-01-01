3372ebd190c8684a1372c4f6265f64106ec78afb
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-01-01 20:42:25 +0900

    add python test script

---
+++ README.md
@@ -0,0 +1,8 @@
+How to run python test script
+
+```
+python3.11 -m venv .env
+source .env/bin/activate
+pip install numpy==1.23 matplotlib dezero
+python dezero_test.py
+```
---
+++ dezero_test.py
@@ -0,0 +1,46 @@
+import dezero
+import dezero.functions as F
+import numpy as np
+
+def p(name, d):
+    print(f"{name}: data: {tuple(d.flatten())}, shape: {d.shape}, strides: {d.strides}")
+
+# reshape
+x = dezero.Variable(np.arange(1, 13).reshape(2, 2, 3))
+y = x.reshape(6, 2)
+y.backward()
+p("reshape y", y.data)
+p("reshape x.grad", x.grad.data)
+print("-------")
+
+# transpose
+x = dezero.Variable(np.arange(1, 13).reshape(2, 2, 3))
+y = x.transpose()
+y.backward()
+p("transpose y", y.data)
+p("transpose x.grad", x.grad.data)
+print("-------")
+
+# broadcast_to
+x = dezero.Variable(np.arange(1, 7).reshape(2, 3))
+y = F.broadcast_to(x, (3, 2, 3))
+y.backward()
+p("broadcast_to y", y.data)
+p("broadcast_to x.grad", x.grad.data)
+print("-------")
+
+# sum
+x = dezero.Variable(np.arange(1, 7).reshape(2, 3))
+y = F.sum(x)
+y.backward()
+p("sum y", y.data)
+p("sum x.grad", x.grad.data)
+print("-------")
+
+# sum_to
+x = dezero.Variable(np.arange(1, 7).reshape(2, 3))
+y = F.sum_to(x, (1, 3))
+y.backward()
+p("sum_to y", y.data)
+p("sum_to x.grad", x.grad.data)
+print("-------")
