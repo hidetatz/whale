32a7d46ba3dbf8b51d05cbc511a2eb6ad260aa8d
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-09 12:13:08 +0900

    improve mnist test program

--- examples/mnist/main.go
+++ examples/mnist/main.go
@@ -4,17 +4,42 @@ import (
 	"os"
 	"encoding/binary"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	lbls, err := readMnistLabel()
+	xi, _, err := readMnistImages()
 	if err != nil {
 		panic(err)
 	}
 
-	for _, lbl := range lbls {
-		fmt.Println(lbl)
+	xl, _, err := readMnistLabels()
+	if err != nil {
+		panic(err)
 	}
+
+	if xi.count != xl.count {
+		panic("unexpected count")
+	}
+
+	for i := range xi.pixels {
+		lbl := xl.labels[i]
+
+		fmt.Printf("%v: ", lbl)
+		for i, pix := range xi.pixels[i] {
+			if i % 28 == 0 {
+				fmt.Printf("\n")
+			}
+			fmt.Printf("%v%s", pix, strings.Repeat(" ", 3 - digits(int(pix))))
+		}
+
+		fmt.Printf("\n\n")
+	}
+}
+
+type MnistImage struct {
+	count int
+	pixels [][]byte
 }
 
 type MnistLabel struct {
@@ -22,49 +47,132 @@ type MnistLabel struct {
 	labels []int
 }
 
-func readMnistLabel() ([]*MnistLabel, error) {
-	files := []string{"train-labels-idx1-ubyte", "t10k-labels-idx1-ubyte"}
+func digits(i int) int {
+    if i == 0 {
+        return 1
+    }
+    count := 0
+    for i != 0 {
+        i /= 10
+        count++
+    }
+    return count
+}
 
-	labels := make([]*MnistLabel, 2)
-	for i, file := range files {
-		f, err := os.Open(fmt.Sprintf("./examples/mnist/%s", file))
+func readMnistImages() (x, t *MnistImage, err error) {
+	fn := func(filename string) (*MnistImage, error) {
+		f, err := os.Open(fmt.Sprintf("./examples/mnist/%s", filename))
 		if err != nil {
 			return nil, err
 		}
 		defer f.Close()
 
-		var magic [4]byte
-		err = binary.Read(f, binary.BigEndian, &magic)
-		if err != nil {
+		magic := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &magic); err != nil {
 			return nil, err
 		}
 
-		m := int(binary.BigEndian.Uint32(magic[:]))
-		if m != 0x00000801 {
+		if int(binary.BigEndian.Uint32(magic)) != 0x00000803 {
 			return nil, fmt.Errorf("unexpected magic")
 		}
 
-		l := &MnistLabel{}
+		mi := &MnistImage{}
 
-		var num [4]byte
-		err = binary.Read(f, binary.BigEndian, &num)
-		if err != nil {
+		num := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &num); err != nil {
 			return nil, err
 		}
 
-		l.count = int(binary.BigEndian.Uint32(num[:]))
-		l.labels = make([]int, l.count)
+		mi.count = int(binary.BigEndian.Uint32(num))
+		mi.pixels = make([][]byte, mi.count)
 
-		for i := 0; i < l.count; i++ {
-			var lbl [1]byte
-			err = binary.Read(f, binary.BigEndian, &lbl)
-			if err != nil {
+		rows := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &rows); err != nil {
+			return nil, err
+		}
+
+		r := int(binary.BigEndian.Uint32(rows))
+
+		cols := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &cols); err != nil {
+			return nil, err
+		}
+
+		c := int(binary.BigEndian.Uint32(cols))
+
+		for i := 0; i < mi.count; i++ {
+			pixel := make([]byte, r * c)
+			if err = binary.Read(f, binary.BigEndian, &pixel); err != nil {
 				return nil, err
 			}
-			l.labels[i] = int(lbl[0])
+
+			mi.pixels[i] = pixel
 		}
-		labels[i] = l
+
+		return mi, nil
 	}
 
-	return labels, nil
+	x, err = fn("train-images-idx3-ubyte")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	t, err = fn("t10k-images-idx3-ubyte")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return x, t, nil
+}
+
+func readMnistLabels() (x, t *MnistLabel, err error) {
+	fn := func(filename string) (*MnistLabel, error) {
+		f, err := os.Open(fmt.Sprintf("./examples/mnist/%s", filename))
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		magic := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &magic); err != nil {
+			return nil, err
+		}
+
+		if int(binary.BigEndian.Uint32(magic)) != 0x00000801 {
+			return nil, fmt.Errorf("unexpected magic")
+		}
+
+		ml := &MnistLabel{}
+
+		num := make([]byte, 4)
+		if err = binary.Read(f, binary.BigEndian, &num); err != nil {
+			return nil, err
+		}
+
+		ml.count = int(binary.BigEndian.Uint32(num))
+		ml.labels = make([]int, ml.count)
+
+		for i := 0; i < ml.count; i++ {
+			var lbl byte
+			if err = binary.Read(f, binary.BigEndian, &lbl); err != nil {
+				return nil, err
+			}
+
+			ml.labels[i] = int(lbl)
+		}
+
+		return ml, nil
+	}
+
+	x, err = fn("train-labels-idx1-ubyte")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	t, err = fn("t10k-labels-idx1-ubyte")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return x, t, nil
 }
