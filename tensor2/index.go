2210e35a106e53b17f4779b4a513d9eb3e272e27
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-20 21:47:30 +0900

    wip implement array indexing

--- tensor2/index.go
+++ tensor2/index.go
@@ -59,4 +59,60 @@ func (t *Tensor) ListIndex(indices [][]int) (*Tensor, error) {
 	}
 
 	// try broadcast each index
+
+	// check if broadcasting is possible
+	size := -1
+	for _, index := range indices {
+		if len(index) == 1 {
+			continue
+		}
+
+		if size == -1 {
+			size = len(index)
+			continue
+		}
+
+		if len(index) != size {
+			return nil, fmt.Errorf("indexing arrays could not be broadcast together with shapes: (%v), (%v)", size, len(index))
+		}
+	}
+
+	// From numpy doc:
+	//     In general, the shape of the resultant array will be
+	//     the concatenation of the shape of the index array
+	//     (or the shape that all the index arrays were broadcast to) with
+	//     the shape of any unused dimensions (those not indexed) in the array being indexed.
+	// https://numpy.org/doc/stable/user/basics.indexing.html#integer-array-indexing
+	newshape := append([]int{size}, t.Shape[len(indices):]...)
+
+	// do actual broadcast
+	for i, index := range indices {
+		if len(index) == 1 {
+			ni := make([]int, size)
+			for j := 0; j < size; j++ {
+				ni[j] = index[0]
+			}
+			indices[i] = ni
+		}
+	}
+
+	accessors := make([][]int, size)
+	for i := 0; i < size; i++ {
+		a := make([]int, len(indices))
+		for j := 0; j < len(indices); j++ {
+			a[j] = indices[j][i]
+		}
+		accessors[i] = a
+	}
+
+	data := []float64{}
+	for _, accessor := range accessors {
+		nt, err := t.Index(accessor...)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, nt.Flatten()...)
+	}
+
+	return Nd(data, newshape...)
 }
