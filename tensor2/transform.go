4005455484e3236bfa7c7cbd2f1badf0b3b083ce
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-22 17:28:49 +0900

    wip implement reshape

---
+++ tensor2/transform.go
@@ -0,0 +1,18 @@
+package tensor2
+
+import "fmt"
+
+func (t *Tensor) Reshape(shape ...int) (*Tensor, error) {
+	if product(shape) != t.Size() {
+		return nil, fmt.Errorf("cannot reshape size %v tensor into %v", t.Size(), shape)
+	}
+
+	t2 := &Tensor{data: t.data, offset: t.offset}
+	t2.Shape = shape
+	strides := make([]int, len(shape))
+	for i := range shape {
+		strides[i] = product(shape[i+1:])
+	}
+	t2.Strides = strides
+	return t2, nil
+}
