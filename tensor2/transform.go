028bc03c6d2e338c1487ad7b03616f62de364985
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-23 22:13:18 +0900

    implement transpose

--- tensor2/transform.go
+++ tensor2/transform.go
@@ -1,6 +1,9 @@
 package tensor2
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func (t *Tensor) Reshape(shape ...int) (*Tensor, error) {
 	if product(shape) != t.Size() {
@@ -15,5 +18,46 @@ func (t *Tensor) Reshape(shape ...int) (*Tensor, error) {
 		strides[i] = product(shape[i+1:])
 	}
 	t2.Strides = strides
+
+	return t2, nil
+}
+
+func (t *Tensor) Transpose(axes ...int) (*Tensor, error) {
+	if t.IsScalar() {
+		return t, nil
+	}
+
+	seq := func(from, to int) []int {
+		r := make([]int, to-from)
+		for i := range r {
+			r[i] = from + i
+		}
+		return r
+	}
+
+	// check
+
+	if len(axes) == 0 {
+		// if axes is empty, create [0, 1, 2...] slice and reverses it
+		axes = seq(0, t.Ndim())
+		slices.Reverse(axes)
+	} else {
+		// else, axes must be an arbitrarily sorted slice of [0, 1, 2, ... dim].
+		copied := make([]int, len(axes))
+		copy(copied, axes)
+		slices.Sort(copied)
+		if !slices.Equal(copied, seq(0, t.Ndim())) {
+			return nil, fmt.Errorf("axes don't much: %v for shape %v", axes, t.Shape)
+		}
+	}
+
+	// do transpose
+
+	t2 := &Tensor{data: t.data, offset: t.offset, Shape: make([]int, t.Ndim()), Strides: make([]int, t.Ndim())}
+	for i, axis := range axes {
+		t2.Shape[i] = t.Shape[axis]
+		t2.Strides[i] = t.Strides[axis]
+	}
+
 	return t2, nil
 }
--- tensor2/transform_test.go
+++ tensor2/transform_test.go
@@ -60,3 +60,103 @@ func TestReshape(t *testing.T) {
 		})
 	}
 }
+
+func TestTranspose(t *testing.T) {
+	tests := []struct {
+		name      string
+		tensor    *Tensor
+		axes      []int
+		expectErr bool
+		expected  *Tensor
+	}{
+		{
+			name:     "scalar",
+			tensor:   Scalar(1),
+			axes:     []int{},
+			expected: Scalar(1),
+		},
+		{
+			name:     "scalar",
+			tensor:   Scalar(1),
+			axes:     []int{0},
+			expected: Scalar(1),
+		},
+		{
+			name:     "vector",
+			tensor:   ArangeVec(1, 13, 1),
+			axes:     []int{},
+			expected: ArangeVec(1, 13, 1),
+		},
+		{
+			name:     "vector",
+			tensor:   ArangeVec(1, 13, 1),
+			axes:     []int{0},
+			expected: ArangeVec(1, 13, 1),
+		},
+		{
+			name:      "axes too big",
+			tensor:    ArangeVec(1, 13, 1),
+			axes:      []int{1},
+			expectErr: true,
+		},
+		{
+			name:      "axes too many",
+			tensor:    ArangeVec(1, 13, 1),
+			axes:      []int{0, 1},
+			expectErr: true,
+		},
+		{
+			name:   "3d",
+			tensor: Must(ArangeVec(1, 25, 1).Reshape(2, 3, 4)),
+			axes:   []int{},
+			expected: MustNd([]float64{
+				1, 13, 5, 17, 9, 21,
+				2, 14, 6, 18, 10, 22,
+				3, 15, 7, 19, 11, 23,
+				4, 16, 8, 20, 12, 24,
+			}, 4, 3, 2),
+		},
+		{
+			name:   "3d 2",
+			tensor: Must(ArangeVec(1, 25, 1).Reshape(2, 3, 4)),
+			axes:   []int{2, 1, 0},
+			expected: MustNd([]float64{
+				1, 13, 5, 17, 9, 21,
+				2, 14, 6, 18, 10, 22,
+				3, 15, 7, 19, 11, 23,
+				4, 16, 8, 20, 12, 24,
+			}, 4, 3, 2),
+		},
+		{
+			name:   "3d 3",
+			tensor: Must(ArangeVec(1, 25, 1).Reshape(2, 3, 4)),
+			axes:   []int{0, 2, 1},
+			expected: MustNd([]float64{
+				1, 5, 9,
+				2, 6, 10,
+				3, 7, 11,
+				4, 8, 12,
+				13, 17, 21,
+				14, 18, 22,
+				15, 19, 23,
+				16, 20, 24,
+			}, 2, 4, 3),
+		},
+		{
+			name:      "non-unique axes",
+			tensor:    Must(ArangeVec(1, 25, 1).Reshape(2, 3, 4)),
+			axes:      []int{0, 2, 0},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := tc.tensor.Transpose(tc.axes...)
+			checkErr(t, tc.expectErr, err)
+			mustEq(t, tc.expected, got)
+		})
+	}
+}
