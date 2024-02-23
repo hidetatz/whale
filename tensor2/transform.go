f8b51ddcc873e2dee487207fab90024e7f93db46
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-23 09:59:16 +0900

    comment for clarity is added

--- tensor2/transform.go
+++ tensor2/transform.go
@@ -7,6 +7,7 @@ func (t *Tensor) Reshape(shape ...int) (*Tensor, error) {
 		return nil, fmt.Errorf("cannot reshape size %v tensor into %v", t.Size(), shape)
 	}
 
+	// reshape shares original tensor data/offset, only shape and strides are modified.
 	t2 := &Tensor{data: t.data, offset: t.offset}
 	t2.Shape = shape
 	strides := make([]int, len(shape))
