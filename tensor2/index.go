23d955616e69e23b4052c542a1e3b4dff2d5003d
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-02-27 09:00:34 +0900

    implement bool

--- tensor2/index.go
+++ tensor2/index.go
@@ -121,3 +121,16 @@ func (t *Tensor) ListIndex(indices [][]int) (*Tensor, error) {
 
 	return NdShape(data, newshape...)
 }
+
+func (t *Tensor) Bool(f func(f float64) bool) *Tensor {
+	c := t.Copy()
+	for i := range c.data {
+		if f(c.data[i]) {
+			c.data[i] = 1
+		} else {
+			c.data[i] = 0
+		}
+	}
+
+	return c
+}
