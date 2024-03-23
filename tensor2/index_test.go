cda15b6f57463f88818bc1e82272cb400544c451
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-03-23 17:08:58 +0900

    unify file

--- tensor2/index_test.go
+++ tensor2/index_test.go
@@ -288,6 +288,210 @@ func TestIndex(t *testing.T) {
 			args:     []*IndexArg{At(1), At(1), At(2), At(3)},
 			expected: Scalar(24),
 		},
+
+		// advanced indexing
+		{
+			name:     "vector 1",
+			tensor:   ArangeVec(0, 6, 1),
+			args:     []*IndexArg{List(ArangeVec(2, 5, 1))},
+			expected: ArangeVec(2, 5, 1),
+		},
+		{
+			name:     "vector 2",
+			tensor:   ArangeVec(0, 6, 1),
+			args:     []*IndexArg{List(MustNew([][]float64{{2, 3, 5}, {0, 1, 4}}))},
+			expected: MustNew([][]float64{{2, 3, 5}, {0, 1, 4}}),
+		},
+		{
+			name:   "too many indices",
+			tensor: ArangeVec(0, 6, 1),
+			args: []*IndexArg{
+				List(MustNew([][]float64{{2, 3, 5}, {0, 1, 4}})),
+				List(MustNew([][]float64{{2, 3, 5}, {0, 1, 4}})),
+			},
+			expectErr: true,
+		},
+		{
+			name:   "2d single list",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(Vector([]float64{0, 1, 0})),
+			},
+			expected: MustNew([][]float64{
+				{0, 1, 2},
+				{3, 4, 5},
+				{0, 1, 2},
+			}),
+		},
+		{
+			name:   "2d multiple lists",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(Vector([]float64{0, 1, 0})),
+				List(Vector([]float64{1, 0, 2})),
+			},
+			expected: Vector([]float64{1, 3, 2}),
+		},
+		{
+			name:   "2d index on 2d",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+			},
+			expected: MustNew([][][]float64{
+				{
+					{0, 1, 2},
+					{3, 4, 5},
+					{0, 1, 2},
+				},
+				{
+					{3, 4, 5},
+					{0, 1, 2},
+					{3, 4, 5},
+				},
+			}),
+		},
+		{
+			name:   "2d broadcast",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+				List(Scalar(2)),
+			},
+			expected: MustNew([][]float64{
+				{2, 5, 2},
+				{5, 2, 5},
+			}),
+		},
+		{
+			name:   "invalid broadcast",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+				List(Vector([]float64{1, 0})),
+			},
+			expectErr: true,
+		},
+		{
+			name:   "slice combined",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				FromTo(0, 2),
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+			},
+			expected: MustNew([][][]float64{
+				{
+					{0, 1, 0},
+					{1, 0, 1},
+				},
+				{
+					{3, 4, 3},
+					{4, 3, 4},
+				},
+			}),
+		},
+		{
+			name:   "slice follows2",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				FromToBy(1, 2, 2),
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+			},
+			expected: MustNew([][][]float64{
+				{
+					{3, 4, 3},
+					{4, 3, 4},
+				},
+			}),
+		},
+		{
+			name:   "slice follows",
+			tensor: Must(Arange(0, 6, 1, 2, 3)),
+			args: []*IndexArg{
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+				FromTo(0, 2),
+			},
+			expected: MustNew([][][]float64{
+				{
+					{0, 1},
+					{3, 4},
+					{0, 1},
+				},
+				{
+					{3, 4},
+					{0, 1},
+					{3, 4},
+				},
+			}),
+		},
+		{
+			name:   "separated",
+			tensor: Must(Arange(0, 48, 1, 2, 3, 4, 2)),
+			args: []*IndexArg{
+				At(1),
+				FromTo(1, 2),
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+			},
+			expected: MustNew([][][][]float64{
+				{
+
+					{
+						{32, 33},
+					},
+					{
+						{34, 35},
+					},
+					{
+						{32, 33},
+					},
+				},
+				{
+					{
+						{34, 35},
+					},
+					{
+						{32, 33},
+					},
+					{
+						{34, 35},
+					},
+				},
+			}),
+		},
+		{
+			name:   "separated2",
+			tensor: Must(Arange(0, 48, 1, 2, 3, 4, 2)),
+			args: []*IndexArg{
+				List(Scalar(1)),
+				FromTo(1, 2),
+				List(MustNew([][]float64{{0, 1, 0}, {1, 0, 1}})),
+			},
+			expected: MustNew([][][][]float64{
+				{
+
+					{
+						{32, 33},
+					},
+					{
+						{34, 35},
+					},
+					{
+						{32, 33},
+					},
+				},
+				{
+					{
+						{34, 35},
+					},
+					{
+						{32, 33},
+					},
+					{
+						{34, 35},
+					},
+				},
+			}),
+		},
 	}
 
 	for _, tc := range tests {
