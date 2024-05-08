3e76fe93ebe503fad17d2e59ca906de885252813
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-05-08 21:19:22 +0900

    implement universal func add.at

---
+++ tensor2/universal_func.go
@@ -0,0 +1,56 @@
+package tensor2
+
+import "fmt"
+
+var (
+	ADD *add
+)
+
+var (
+	_ universalfunc = &add{}
+)
+
+type universalfunc interface {
+	At(x *Tensor, indices []*IndexArg, target *Tensor) error
+}
+
+type add struct{}
+
+func (a *add) At(x *Tensor, indices []*IndexArg, target *Tensor) error {
+	return ufuncAt(x, indices, func(orig, arg float64) float64 { return orig + arg }, target)
+}
+
+func ufuncAt(x *Tensor, indices []*IndexArg, fn func(orig, arg float64) float64, target *Tensor) error {
+	if x.IsScalar() {
+		return fmt.Errorf("index is not defined on scalar %v", x)
+	}
+
+	if len(indices) == 0 {
+		return fmt.Errorf("index accessor must not be empty")
+	}
+
+	if x.Ndim() < len(indices) {
+		return fmt.Errorf("too many index accessors specified: %v", indices)
+	}
+
+	r, err := x.index(indices...)
+	if err != nil {
+		return err
+	}
+
+	tgt, err := target.BroadcastTo(r.t.Shape...)
+	if err != nil {
+		return fmt.Errorf("operands could not broadcast together with shapes %v, %v", target.Shape, r.t.Shape)
+	}
+
+	it := tgt.Iterator()
+	i := 0
+	for it.HasNext() {
+		tg := it.Next()
+		idx := r.origIndices[i]
+		x.data[idx] = fn(x.data[idx], tg)
+		i++
+	}
+
+	return nil
+}
---
+++ tensor2/universal_func_test.go
@@ -0,0 +1,149 @@
+package tensor2
+
+import "testing"
+
+func TestAt_ADD(t *testing.T) {
+	tests := []struct {
+		name      string
+		x         *Tensor
+		indices   []*IndexArg
+		target    *Tensor
+		expected  *Tensor
+		expectErr bool
+	}{
+		{
+			name:     "error empty",
+			x:        ArangeVec(3, 6, 1),
+			indices:  []*IndexArg{At(0)},
+			target:   Scalar(1),
+			expected: Vector([]float64{4, 4, 5}),
+		},
+		{
+			name:      "scalar error",
+			x:         Scalar(3),
+			indices:   []*IndexArg{At(0)},
+			target:    Scalar(1),
+			expectErr: true,
+			expected:  Scalar(3),
+		},
+		{
+			name:     "vector",
+			x:        Vector([]float64{1, 2, 3, 4, 5}),
+			indices:  []*IndexArg{At(0)},
+			target:   Scalar(1),
+			expected: Vector([]float64{2, 2, 3, 4, 5}),
+		},
+		{
+			name: "vector 2",
+			x:    Vector([]float64{1, 2, 3, 4, 5}),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1, 2})),
+			},
+			target:   Scalar(1),
+			expected: Vector([]float64{2, 3, 4, 4, 5}),
+		},
+		{
+			name: "vector 3",
+			x:    Vector([]float64{1, 2, 3, 4, 5}),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1, 2})),
+			},
+			target:   Vector([]float64{1, 2, 3}),
+			expected: Vector([]float64{2, 4, 6, 4, 5}),
+		},
+		{
+			name: "vector err",
+			x:    Vector([]float64{1, 2, 3, 4, 5}),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1, 2})),
+				List(Vector([]float64{0, 1, 2})),
+			},
+			target:    Scalar(1),
+			expectErr: true,
+			expected:  Vector([]float64{1, 2, 3, 4, 5}),
+		},
+		{
+			name: "vector err2",
+			x:    Vector([]float64{1, 2, 3, 4, 5}),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1, 2})),
+			},
+			target:    Vector([]float64{1, 2}),
+			expectErr: true,
+			expected:  Vector([]float64{1, 2, 3, 4, 5}),
+		},
+		{
+			name: "2d",
+			x:    Must(Arange(1, 7, 1, 2, 3)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 0, 1})),
+			},
+			target:   Scalar(1),
+			expected: Must(NdShape([]float64{3, 4, 5, 5, 6, 7}, 2, 3)),
+		},
+		{
+			name: "2d 2",
+			x:    Must(Arange(1, 7, 1, 2, 3)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1})),
+				List(Vector([]float64{1, 2})),
+			},
+			target:   Scalar(1),
+			expected: Must(NdShape([]float64{1, 3, 3, 4, 5, 7}, 2, 3)),
+		},
+		{
+			name: "2d 3",
+			x:    Must(Arange(1, 7, 1, 2, 3)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1})),
+				List(Vector([]float64{1, 2})),
+			},
+			target:   Vector([]float64{1, 2}),
+			expected: Must(NdShape([]float64{1, 3, 3, 4, 5, 8}, 2, 3)),
+		},
+		{
+			name: "2d err",
+			x:    Must(Arange(1, 7, 1, 2, 3)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1})),
+				List(Vector([]float64{1, 2})),
+				List(Vector([]float64{0, 0})),
+			},
+			target:    Scalar(1),
+			expectErr: true,
+			expected:  Must(Arange(1, 7, 1, 2, 3)),
+		},
+		{
+			name: "2d err 2",
+			x:    Must(Arange(1, 7, 1, 2, 3)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1})),
+				List(Vector([]float64{1, 2})),
+			},
+			target:    Vector([]float64{1, 2, 3}),
+			expectErr: true,
+			expected:  Must(Arange(1, 7, 1, 2, 3)),
+		},
+		{
+			name: "3d",
+			x:    Must(Arange(1, 13, 1, 2, 3, 2)),
+			indices: []*IndexArg{
+				List(Vector([]float64{0, 1})),
+				List(Vector([]float64{1, 2})),
+				List(Vector([]float64{1, 0})),
+			},
+			target:   Vector([]float64{1, 2}),
+			expected: Must(NdShape([]float64{1, 2, 3, 5, 5, 6, 7, 8, 9, 10, 13, 12}, 2, 3, 2)),
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			err := ADD.At(tc.x, tc.indices, tc.target)
+			checkErr(t, tc.expectErr, err)
+			mustEq(t, tc.expected, tc.x)
+		})
+	}
+}
