a182b7a292410ae100583f2d69b5ff809c6ede9c
Hidetatz Yaginuma <hidetatz@gmail.com>
2024-05-12 08:05:51 +0900

    profiling

--- examples/mlp/main.go
+++ examples/mlp/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/pprof"
 
 	"github.com/hidetatz/whale"
 	tensor "github.com/hidetatz/whale/tensor2"
 )
-
-// var device = &whale.CPU{}
 
 func Predict(m whale.Model, x *whale.Variable) *whale.Variable {
 	pred, err := m.Train(x)
@@ -21,7 +21,7 @@ func Train(m whale.Model, x, y *whale.Variable) {
 	lossCalc := m.Loss()
 	optim := m.Optimizer()
 
-	for i := range 10000 {
+	for i := range 3000 {
 		pred, err := m.Train(x)
 		if err != nil {
 			panic(err)
@@ -54,6 +54,19 @@ func Train(m whale.Model, x, y *whale.Variable) {
 }
 
 func main() {
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err := pprof.StartCPUProfile(f); err != nil {
+		panic(err)
+	}
+	defer pprof.StopCPUProfile()
 	xt, yt := whale.RandSin(100, 1)
 	x := whale.NewVar(xt)
 	y := whale.NewVar(yt)
