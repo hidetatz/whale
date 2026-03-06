0063ffa375caf311e9486a10d217d7c5a9073025
Hidetatz Yaginuma <hidetatz@gmail.com>
2026-03-06 14:19:49 +0900

    dtype

--- backend2.py
+++ backend2.py
@@ -2,26 +2,44 @@ import os
 import time
 from dataclasses import dataclass
 from typing import Any
-from ctypes import CDLL, byref, c_float, c_int, c_uint, c_longlong, c_void_p, cast, pointer, sizeof
-from enum import IntEnum, auto
+from ctypes import CDLL, byref, c_double, c_float, c_int, c_longlong, c_void_p, cast, pointer, sizeof
 
-class DType(IntEnum):
-    I32 = auto()
-    I64 = auto()
-    F32 = auto()
-    F64 = auto()
+class DType:
+    pass
+
+class Int32(DType):
+    def ctype(self): return c_int
+
+class Int64(DType):
+    def ctype(self): return c_longlong
+
+class Float32(DType):
+    def ctype(self): return c_float
+
+class Float64(DType):
+    def ctype(self): return c_double
+
+int32 = Int32()
+int64 = Int64()
+float32 = Float32()
+float64 = Float64()
 
 #
 # buffer
 # 
 
-@dataclass
 class CPUBuff:
-    val: list[float] = None
+    def __init__(self, val=None, dtype=None):
+        self.val = val
+        self.dtype = dtype
+        if val and dtype is None:
+            assert type(val[0]) is int or type(val[0]) is float
+            self.dtype = int64 if type(val[0]) is int else float64
 
-@dataclass
 class DevBuff:
-    ptr: c_void_p = None
+    def __init__(self, ptr=None, dtype=None):
+        self.ptr = ptr
+        self.dtype = dtype
 
 @dataclass
 class Kernel:
@@ -62,20 +80,22 @@ class CUDA(Backend):
 
     def name(self): return "cuda"
 
-    def memalloc(self, size):
+    def memalloc(self, length, dtype):
         ptr = c_void_p()
-        self.cuda("cuMemAlloc", byref(ptr), size)
+        self.cuda("cuMemAlloc", byref(ptr), sizeof(dtype.ctype()) * length)
         return ptr
 
     def free(self, ptr):
         self.cuda("cuMemFree", ptr)
 
-    def memcpy_htod(self, dst, val, size, length):
-        self.cuda("cuMemcpyHtoD", dst, (c_longlong * length)(*val), size)
+    def memcpy_htod(self, dst, val, length, dtype):
+        ctype = dtype.ctype()
+        self.cuda("cuMemcpyHtoD", dst, (ctype * length)(*val), sizeof(ctype) * length)
 
-    def memcpy_dtoh(self, src, size, length):
-        out = (c_longlong * length)()
-        self.cuda("cuMemcpyDtoH", out, src, size)
+    def memcpy_dtoh(self, src, length, dtype):
+        ctype = dtype.ctype()
+        out = (ctype * length)()
+        self.cuda("cuMemcpyDtoH", out, src, sizeof(ctype) * length)
         return [out[i] for i in range(length)]
 
     def compile(self, name, src):
@@ -130,20 +150,18 @@ cuda = CUDA()
 @dataclass
 class MemallocD:
     dst: DevBuff
-    size: int
+    length: int
 
 @dataclass
 class MemcpyHtoD:
     dst: DevBuff
     src: CPUBuff
-    size: int
     length: int
 
 @dataclass
 class MemcpyDtoH:
     dst: CPUBuff
     src: DevBuff
-    size: int
     length: int
 
 @dataclass
@@ -158,8 +176,8 @@ class BackendIR:
 
 sum2d_axis0 = """
 extern "C" __global__ void sum2d_axis0(
-    int64_t* in0,
-    int64_t* out,
+    double* in0,
+    double* out,
     int out_size,
     int in0_stride0,
     int in0_stride1,
@@ -177,10 +195,10 @@ extern "C" __global__ void sum2d_axis0(
 
 fuse1d_add_mul = """
 extern "C" __global__ void fuse1d_add_mul(
-    int64_t* in0,
-    int64_t* in1,
-    int64_t* in2,
-    int64_t* out,
+    double* in0,
+    double* in1,
+    double* in2,
+    double* out,
     int out_size
 ) {
     int out_idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -201,11 +219,11 @@ class Executor:
         for step in steps:
             match step:
                 case MemallocD():
-                    step.dst.ptr = self.backend.memalloc(step.size)
+                    step.dst.ptr = self.backend.memalloc(step.length, step.dst.dtype)
                 case MemcpyHtoD():
-                    self.backend.memcpy_htod(step.dst.ptr, step.src.val, step.size, step.length)
+                    self.backend.memcpy_htod(step.dst.ptr, step.src.val, step.length, step.dst.dtype)
                 case MemcpyDtoH():
-                    step.dst.val = self.backend.memcpy_dtoh(step.src.ptr, step.size, step.length)
+                    step.dst.val = self.backend.memcpy_dtoh(step.src.ptr, step.length, step.dst.dtype)
                 case InvokeKernel():
                     kern = self.kernels[step.name]
                     self.backend.invoke(kern.ptr, (1, 1, 1), (32, 1, 1), step.params)
@@ -219,33 +237,33 @@ if __name__ == "__main__":
     # e = b + d
     # f = e * c
     # f.materialize()
-    a = CPUBuff([1, 2, 3, 1, 2, 3])
-    a_dev = DevBuff()
+    a = CPUBuff([1.0, 2.0, 3.0, 1.0, 2.0, 3.0])
+    a_dev = DevBuff(dtype=a.dtype)
 
-    b = CPUBuff([4, 5, 6])
-    b_dev = DevBuff()
+    b = CPUBuff([4.0, 5.0, 6.0])
+    b_dev = DevBuff(dtype=b.dtype)
 
-    c = CPUBuff([10, 10, 10])
-    c_dev = DevBuff()
+    c = CPUBuff([10.0, 10.0, 10.0])
+    c_dev = DevBuff(dtype=c.dtype)
 
-    d_dev = DevBuff()
-    f_dev = DevBuff()
-    f = CPUBuff()
+    d_dev = DevBuff(dtype=a_dev.dtype)
+    f_dev = DevBuff(dtype=d_dev.dtype)
+    f = CPUBuff(dtype=f_dev.dtype)
 
     ir = BackendIR(
         backend=cuda,
         steps=[
-            MemallocD(dst=a_dev, size=6*8),
-            MemcpyHtoD(dst=a_dev, src=a, size=6*8, length=6),
-            MemallocD(dst=b_dev, size=3*8),
-            MemcpyHtoD(dst=b_dev, src=b, size=3*8, length=3),
-            MemallocD(dst=c_dev, size=3*8),
-            MemcpyHtoD(dst=c_dev, src=c, size=3*8, length=3),
-            MemallocD(dst=d_dev, size=3*8),
+            MemallocD(dst=a_dev, length=6),
+            MemcpyHtoD(dst=a_dev, src=a, length=6),
+            MemallocD(dst=b_dev, length=3),
+            MemcpyHtoD(dst=b_dev, src=b, length=3),
+            MemallocD(dst=c_dev, length=3),
+            MemcpyHtoD(dst=c_dev, src=c, length=3),
+            MemallocD(dst=d_dev, length=3),
             InvokeKernel(name="sum2d_axis0", params=[a_dev, d_dev, 3, 3, 1, 2]),
-            MemallocD(dst=f_dev, size=3*8),
+            MemallocD(dst=f_dev, length=3),
             InvokeKernel(name="fuse1d_add_mul", params=[b_dev, d_dev, c_dev, f_dev, 3]),
-            MemcpyDtoH(src=f_dev, dst=f, size=3*8, length=3),
+            MemcpyDtoH(src=f_dev, dst=f, length=3),
         ],
     )
 
